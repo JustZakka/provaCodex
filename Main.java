2e4814148612bca441fbf375b7a61a4dbfd081cd
ZakBoum <zakaria.boumchita@mail.polimi.it>
2024-03-07 15:24:36 +0100

    Sistemati definitivamente tavolo, slot, giocatore

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        Tavolo tavolo = new Tavolo(20, 20);
+        Tavolo tavolo = new Tavolo(3, 3);
+        Giocatore p1 = new Giocatore(tavolo);
+
+        p1.analizzaTavolo();
+        p1.piazzaCartaIniziale(1, 2);
+        p1.analizzaTavolo();
 
     }
 }
--- Tavolo.java
+++ Tavolo.java
@@ -1,13 +1,27 @@
 public class Tavolo {
 
-    private Slot[][] slots;
+    private final Slot[][] slots;
+    private final int r;
+    private final int c;
 
     public Tavolo(int r, int c) {
+        this.r = r;
+        this.c = c;
         this.slots = new Slot[r][c];
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 slots[i][j] = new Slot();
             }
+        }
+    }
+
+    public void printTavolo() {
+        System.out.println("-------------------------");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print("[" + slots[i][j].getSlotOccupato() + "]");
+            }
+            System.out.println();
         }
     }
 
