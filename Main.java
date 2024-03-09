1c60f853bc345f2d984320b96d54a61d24ddabb1
ZakBoum <zakaria.boumchita@mail.polimi.it>
2024-03-09 21:26:28 +0100

    SISTEMATO MAIN

--- Main.java
+++ Main.java
@@ -32,37 +32,52 @@ public class Main {
         sceltaContinua = scanner.nextInt();
 
         while (sceltaContinua != -1) {
-            System.out.println(":::Mazzo di p1:::");
-            p1.displayMazzo();
-            System.out.println("Seleziona il numero della carta che vuoi piazzare: ");
-            sceltaCarta = scanner.nextInt();
-            System.out.println("Seleziona se vuoi piazzarla di fronte o retro: 1) -> Fronte | 0) -> Retro");
-            sceltaFronte = scanner.nextInt();
-            System.out.println("Seleziona la riga della carta a cui vuoi attaccarti:");
-            p1.getTavoloGiocatore().printTavolo();
-            riga = scanner.nextInt();
-            System.out.println("Seleziona la colonna della carta a cui vuoi attaccarti:");
-            colonna = scanner.nextInt();
-            System.out.println("Carta selezionata sul tavolo:");
-            if (p1.getTavoloGiocatore().getSlots()[riga][colonna].getCartaSlot().getPiazzataInFronte() == 1) {
-                p1.getTavoloGiocatore().getSlots()[riga][colonna].getCartaSlot().printAngoliFronte();
-            } else {
-                p1.getTavoloGiocatore().getSlots()[riga][colonna].getCartaSlot().printAngoliRetro();
-            }
-            System.out.println("Carta da piazzare: ");
-            if (sceltaFronte == 1) {
-                p1.getMazzoGiocatore().getCarte().get(sceltaCarta-1).printAngoliFronte();
-            } else {
-                p1.getMazzoGiocatore().getCarte().get(sceltaCarta-1).printAngoliRetro();
-            }
-            System.out.println("Seleziona l'angolo della carta sul tavolo a cui vuoi attaccarti (a partire da in alto a dx in senso orario 0->3): ");
-            sceltaAngolo = scanner.nextInt();
-            p1.piazzaCarta(riga, colonna, p1.getMazzoGiocatore().getCarte().get(sceltaCarta-1), sceltaFronte, sceltaAngolo);
-            p1.getTavoloGiocatore().printTavolo();
 
-            System.out.println("Vuoi continuare ?");
-            System.out.println("Sì: 1");
-            System.out.println("No: -1");
+            System.out.println("Cosa vuoi fare ?");
+            System.out.println("1) Piazza una Carta");
+            System.out.println("2) Analizza il tavolo");
+            sceltaContinua = scanner.nextInt();
+
+            if (sceltaContinua == 1) {
+                System.out.println(":::Mazzo di p1:::");
+                p1.displayMazzo();
+                System.out.println("Seleziona il numero della carta che vuoi piazzare: ");
+                sceltaCarta = scanner.nextInt();
+                System.out.println("Seleziona se vuoi piazzarla di fronte o retro: 1) -> Fronte | 0) -> Retro");
+                sceltaFronte = scanner.nextInt();
+                System.out.println("Seleziona la riga della carta a cui vuoi attaccarti:");
+                p1.getTavoloGiocatore().printTavolo();
+                riga = scanner.nextInt();
+                System.out.println("Seleziona la colonna della carta a cui vuoi attaccarti:");
+                colonna = scanner.nextInt();
+                System.out.println("Carta selezionata sul tavolo:");
+                if (p1.getTavoloGiocatore().getSlots()[riga][colonna].getCartaSlot().getPiazzataInFronte() == 1) {
+                    p1.getTavoloGiocatore().getSlots()[riga][colonna].getCartaSlot().printAngoliFronte();
+                } else {
+                    p1.getTavoloGiocatore().getSlots()[riga][colonna].getCartaSlot().printAngoliRetro();
+                }
+                System.out.println("Carta da piazzare: ");
+                if (sceltaFronte == 1) {
+                    p1.getMazzoGiocatore().getCarte().get(sceltaCarta-1).printAngoliFronte();
+                } else {
+                    p1.getMazzoGiocatore().getCarte().get(sceltaCarta-1).printAngoliRetro();
+                }
+                System.out.println("Seleziona l'angolo della carta sul tavolo a cui vuoi attaccarti (a partire da in alto a dx in senso orario 0->3): ");
+                sceltaAngolo = scanner.nextInt();
+                p1.piazzaCarta(riga, colonna, p1.getMazzoGiocatore().getCarte().get(sceltaCarta-1), sceltaFronte, sceltaAngolo);
+                p1.getTavoloGiocatore().printTavolo();
+            } else if (sceltaContinua == 2) {
+                p1.analisiTavolo();
+                System.out.println("Scegli la riga della carta che vuoi analizzare: ");
+                riga = scanner.nextInt();
+                System.out.println("Scegli la colonna della carta che vuoi analizzare: ");
+                colonna = scanner.nextInt();
+                p1.getTavoloGiocatore().analisiCartaTavolo(riga, colonna);
+            }
+
+            System.out.println("Cosa vuoi fare ?");
+            System.out.println("-1: Chiudi il gioco");
+            System.out.println("1: Vai avanti col gioco");
             sceltaContinua = scanner.nextInt();
         }
     }
